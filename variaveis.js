31ef9c4ce7e2574d84debc5d515e26b4648c33c6
Caiocac <88864750+Caiocacxd@users.noreply.github.com>
2022-08-24 20:48:36 +0000

    Atualizacao arquivo .js

---
+++ variaveis.js
@@ -0,0 +1,10 @@
+//Abstracao de uma Arvore do tipo coqueiro
+var altura = 20; //number(inteiro)
+var largura = 1.5; //number(float)
+var tipomadeira = "estipe"; //String
+var cormadeira = "ouro claro"; //String
+var tipofolha = "penada" ; //String
+var corfolhas = "verde-amarelada" ; //String
+var especie = "Coqueiro" //String
+var flor = false; //boolean
+var fruto = true; //boolean
