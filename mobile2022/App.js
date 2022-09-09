1be38ed8ece23c33bb947b8fbc799fb4109573f9
Caiocacxd <caio-cac@hotmail.com>
2022-09-08 21:18:11 -0300

    Aula 08/09

--- mobile2022/App.js
+++ mobile2022/App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
+import rng from './assets/rng.jpg';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+   <>
+   <View style={styles.container}>
+      <Text style={styles.firstText}>Primeira pagina </Text>
+      <Text style={styles.secondText}>Desenvolvimento</Text>
+      <Image
+        style={styles.tinyLogo}
+        source={rng}
+      ></Image>
+      <Image
+        style={styles.tinyLogo}
+        source={{uri: 'https://resultadosdigitais.com.br/files/2015/08/thestocks-imagem.jpg'}}
+      ></Image>
       <StatusBar style="auto" />
     </View>
+  </>
   );
 }
 
@@ -17,4 +30,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  firstText: {
+    color: 'black',
+    backgroundColor: 'red',
+    fontSize: 50
+  },
+  secondText: {
+    color: 'red',
+    backgroundColor: 'black',
+    fontSize: 20
+  },
+  tinyLogo: {
+    width: 100,
+    height: 100
+  }
 });
